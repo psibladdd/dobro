08c5a6b8b7c3a343d57dbc8ba0d35645961bec89
Dima <151432491+psibladdd@users.noreply.github.com>
2025-07-23 17:04:02 +0300

    Update secondpage.jsx

--- src/secondpage.jsx
+++ src/secondpage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import './SecondPage.css'; // Импортируем CSS файл для стилей
 
 const SecondPage = () => {
   const { word } = useParams();
