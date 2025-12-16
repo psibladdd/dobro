5836349710143c7fa3ace9ebf21250888591a960
Dima <151432491+psibladdd@users.noreply.github.com>
2025-12-16 03:53:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage';
-import SecondPage from './secondpage';
+import GameBoard from './Start.jsx';
 import './App.css';
 
-const App = () => {
+function App() {
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:word" element={<SecondPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <GameBoard />
+    </div>
   );
-};
+}
 
 export default App;
